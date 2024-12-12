f11882466f9def9479d5d44788b8502dbc084ce9
EricScottJohnston <ericjohnston105@gmail.com>
2024-12-12 14:21:21 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Clean the build cache
+  cleanDistDir: true
+}
+
+module.exports = nextConfig
 import { Icon } from 'lucide-react';
 import { MessageSquare } from 'lucide-react';
 future: { webpack5: true }
