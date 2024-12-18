5cc18bda4ca4d07b3da3e875049252b619e38c67
EricScottJohnston <ericjohnston105@gmail.com>
2024-12-18 09:14:50 -0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -6,7 +7,10 @@ const inter = Inter({ subsets: ['latin'] })
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
