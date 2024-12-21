2e48b5ccefd2fde1955445f6e483cce86e33f75e
EricScottJohnston <ericjohnston105@gmail.com>
2024-12-21 16:21:41 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,9 @@
-module.exports = {}
+// next.config.js
+const { withContentlayer } = require('next-contentlayer')
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // your existing config
+}
+
+module.exports = withContentlayer(nextConfig)
