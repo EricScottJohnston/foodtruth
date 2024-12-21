7b9283afc6ef5272f3ee52db862bdcac265557fd
EricScottJohnston <ericjohnston105@gmail.com>
2024-12-21 16:17:44 -0700

    Create contentlayer.config.js

---
+++ contentlayer.config.js
@@ -0,0 +1,37 @@
+// contentlayer.config.js
+import { defineDocumentType, makeSource } from 'contentlayer/source-files'
+
+export const Post = defineDocumentType(() => ({
+  name: 'Post',
+  filePathPattern: `**/*.mdx`,
+  contentType: 'mdx',
+  fields: {
+    title: {
+      type: 'string',
+      required: true,
+    },
+    publishedAt: {
+      type: 'date',
+      required: true,
+    },
+    summary: {
+      type: 'string',
+      required: true,
+    },
+    image: {
+      type: 'string',
+      required: false,
+    },
+  },
+  computedFields: {
+    slug: {
+      type: 'string',
+      resolve: (post) => `/blog/${post._raw.flattenedPath}`,
+    },
+  },
+}))
+
+export default makeSource({
+  contentDirPath: 'content/posts',
+  documentTypes: [Post],
+})
